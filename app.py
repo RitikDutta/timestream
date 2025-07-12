d7881e195159da9faa272439417bb190eda788ba
ritik dutta <ritikduttagd@gmail.com>
2025-07-12 12:59:07 +0530

    added a new route for direct frame in yt

--- app.py
+++ app.py
@@ -8,14 +8,32 @@ timestream = Timestream()
 def index():
     return render_template('index.html')
 
-@app.route('/display', methods=['POST'])
+@app.route('/display', methods=['GET', 'POST'])
 def display():
-    youtube_url = request.form['youtube_url']
+    if request.method == 'POST':
+        youtube_url = request.form['youtube_url']
+    else:  # GET request
+        youtube_url = request.args.get('url')
     video_id = timestream.get_youtube_video_id(youtube_url)
     transcript = timestream.get_youtube_transcript(video_id)
     meaningful_timestamps = timestream.generate_meaningful_timestamps(transcript)
     embed_url = f"https://www.youtube.com/embed/{video_id}"
     return render_template('display.html', youtube_url=embed_url, transcript=meaningful_timestamps)
+
+@app.route('/display_frame', methods=['GET', 'POST'])
+def display_frame():
+    if request.method == 'POST':
+        youtube_url = request.form['youtube_url']
+        print("youtube_url", youtube_url)
+    else:  # GET request
+        youtube_url = request.args.get('url')
+    video_id = timestream.get_youtube_video_id(youtube_url)
+    # video_id = 'tNrNLoCqzco'
+
+    transcript = timestream.get_youtube_transcript(video_id)
+    meaningful_timestamps = timestream.generate_meaningful_timestamps(transcript)
+    embed_url = f"https://www.youtube.com/embed/{video_id}"
+    return render_template('display_frame.html', youtube_url=embed_url, transcript=meaningful_timestamps)
 
 @app.route('/reload_timestamps', methods=['POST'])
 def reload_timestamps():
@@ -38,9 +56,10 @@ def user_query_endpoint():
     transcript = timestream.get_youtube_transcript(video_id)
     # Use the user_query function defined in Timestream
     answer = timestream.user_query(transcript, user_query)
+    
     print(f'ANSWER======================={answer}')
     # Return JSON response
     return jsonify({'response': answer})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
